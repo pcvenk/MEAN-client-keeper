69ba6cbec0d6863154c3c81811e5c99f8779cfb9
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-01-09 15:53:41 +0100

    Set up of the required middleware

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var mongojs = require("mongojs");
+
+//Set up static folder)
+app.use(express.static(__dirname+'/public'));
+
+//Set up bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
    res.send('Hello World');
