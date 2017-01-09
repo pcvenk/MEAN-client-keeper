5ca6eef38cfe9eb4eb5829f00628f4575dd53e02
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-01-09 17:21:36 +0100

    Initialized mongojs, added some data to it and set up the /client route to get db data

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 var mongojs = require("mongojs");
+//mongojs setup (first parameter is the db name, second the coll name)
+var db = mongojs('client-keeper', ['clients']);
 
 //Set up static folder)
 app.use(express.static(__dirname+'/public'));
@@ -10,8 +12,16 @@ app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-   res.send('Hello World');
+app.get('/clients', function(req, res){
+   db.clients.find(function(err, docs){
+      if(err){
+         console.log('Request sent');
+         res.send(err);
+      } else {
+         console.log('Sending data...');
+         res.send(docs);
+      }
+   });
 });
 
 app.listen(3000);
